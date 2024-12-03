ef5531b2b0e7abf2bb875681d0edd6ac21fdff0e
cronox808 <187470821+cronox808@users.noreply.github.com>
2024-12-03 08:05:39 -0500

    temas vistos: variables, listas

---
+++ main.py
@@ -0,0 +1,16 @@
+name = "Jose David Pabon"
+age = 17
+heigth = 1.78
+studient = True
+#conjuntos
+
+hobbies = list()
+hobbies = ["cocinar", "programar", "dormir", "musica"]
+#hobbies[2] = "despertarme"
+
+#dolocitar un nuevo dato para la lista
+#dato = input("ingrese el nuevo pasatiempo: ")
+#hobbies.append(dato)
+hobbies.pop()
+print(hobbies)
+
